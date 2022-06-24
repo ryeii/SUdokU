1f52a8d40802d9997098e3b0ae1e0a0d1e2837f4
ryeii <anzhiyu@berkeley.edu>
2022-06-23 18:52:12 -0700

    r

--- blocked_mask_solve.py
+++ blocked_mask_solve.py
@@ -17,9 +17,6 @@ def to_mat(quiz):
     return np.array([[int(quiz[i * 9 + j]) for j in range(9)] for i in range(9)])
 
 
-def to_str(mat):
-    return ''.join(''.join(str(mat.tolist()[i])) for i in range(9))
-
 # Takes a string of 81 characters and returns a list of 9x9 matrices, each representing a 3x3 sub-grid.
 def to_sub_grids(quiz):
     mat = to_mat(quiz)
@@ -30,4 +27,13 @@ def to_sub_grids(quiz):
                 if j in [0, 3, 6]:
                     sub_grids.append(mat[i:i + 3, j:j + 3])
     return sub_grids
+
+# Takes a list of 9x9 matrices and returns a string of 81 characters, representing the board.
+def to_quiz(sub_grids):
+    quiz = ''
+    for i in range(9):
+        for j in range(9):
+            quiz += str(sub_grids[i][j].tolist()[0])
+    return quiz
+
 
