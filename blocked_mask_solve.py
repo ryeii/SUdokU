3803cccfc2342afd932c4c1ec920b3b4cfc5787d
ryeii <anzhiyu@berkeley.edu>
2022-06-27 16:50:15 -0700

    minor changes

--- blocked_mask_solve.py
+++ blocked_mask_solve.py
@@ -2,18 +2,6 @@ from re import sub
 import numpy as np
 
 
-def display(board_str):
-    board_str = to_str(board_str)
-    for i in range(9):
-        if i in [3, 6]:
-            print('------+-------+------')
-        for j in range(9):
-            if j in [3, 6]:
-                print('| ', end='')
-            print(board_str[i * 9 + j] + ' ', end='')
-        print()
-
-
 def to_mat(quiz):
     return np.array([[int(quiz[i * 9 + j]) for j in range(9)] for i in range(9)])
 
