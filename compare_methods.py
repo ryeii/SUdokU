b7dfe4994de5778b57b715d916f80754e68bc9a4
ryeii <anzhiyu@berkeley.edu>
2022-06-29 15:40:40 -0700

    added solution checker

--- compare_methods.py
+++ compare_methods.py
@@ -7,7 +7,7 @@ import dancing_link
 import mask_solve
 import blocked_mask_solve
 
-quiz_df = pd.read_csv("sudoku.csv", sep=',')
+quiz_df = pd.read_csv("sudoku_cluewise.csv", sep=',')
 print(quiz_df.size)
 
 
@@ -22,16 +22,27 @@ def display(board):
         print()
 
 
-def plot_clue_size_distribution(quiz_df, size):
-    plt.figure(figsize=(5, 5))
-    plt.title("Clue size distribution")
-    plt.xlabel("Clue size")
-    plt.ylabel("Number of quizzes")
-    clue_size = []
-    for i in range(size):
-        clue_size.append(81 - quiz_df["quizzes"][i].count('0'))
-    plt.hist(clue_size, bins=range(10, 60, 10))
-    plt.show()
+# In case there is multiple answers for a quiz, here is a function to check whether a solution is valid
+def check(sol):
+    grid = [sol[i: i + 9] for i in [0, 9, 18, 27, 36, 45, 54, 63, 72]]
+    for i in grid:
+        if not len(list(set(i))) == 9:
+            return False
+    for i in range(9):
+        l = []
+        for j in grid:
+            l.append(j[i])
+        if not len(list(set(l))) == 9:
+            return False
+    for i in [0, 3, 6]:
+        for j in [0, 3, 6]:
+            block = []
+            for k in range(3):
+                for l in range(3):
+                    block.append(grid[i + l][j + k])
+            if not len(list(set(block))) == 9:
+                return False
+    return True
 
 
 def run_test(m, size):
@@ -60,8 +71,8 @@ def run_test(m, size):
 # print()
 # display(quiz_df["solutions"][0])
 
-methods = [mask_solve.solve, blocked_mask_solve.solve, dancing_link.sol]
-correct, size, solving_time = run_test(methods, 2000)
+methods = [dancing_link.sol]
+correct, size, solving_time = run_test(methods, 192000)
 for i in range(len(methods)):
     print("method", str(i), "solved", str(correct[i]), "out of", str(size), "puzzles,", str(correct[i] / size * 100),
           "%. avg solving time: ",
